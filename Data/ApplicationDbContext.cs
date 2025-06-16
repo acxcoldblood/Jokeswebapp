77e398d3d3efd06c010e8b3c9652548ba70bc17d
Kushagra <kush.bholu@gmail.com>
2025-06-17 03:54:54 +0530

    Initial commit - jokeswebapp

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using jokeswebapp.Models;
+
+namespace jokeswebapp.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<jokeswebapp.Models.Joke> Joke { get; set; } = default!;
+    }
+}
---
+++ Models/Joke.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace jokeswebapp.Models
+{
+    public class Joke
+    {
+        [Key]
+        public int Id { get; set; }  // ✅ Primary Key
+
+        [Required]
+        public string JokeQuestion { get; set; }
+
+        [Required]
+        public string JokeAnswer { get; set; }
+    }
+}
